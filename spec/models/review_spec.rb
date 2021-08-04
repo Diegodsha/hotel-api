adfcb6861eada7a16dae9d52a17f4a159b4e491e
Diego <diegodsha@hotmail.com>
2021-08-04 15:56:07 -0500

    Add review model tests

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:name) { 'simple name' }
+
+  it { should belong_to(:hotel) } 
+  it { should validate_presence_of(:title) }
+
 end
