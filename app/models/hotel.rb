dc3c98ef7bafffe65744de7387f3f54bfdf6fc5a
Diego <diegodsha@hotmail.com>
2021-08-04 15:54:31 -0500

    Add validations to models

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,3 +1,10 @@
 class Hotel < ApplicationRecord
-    has_many :reviews
+    has_many :reviews, dependent: :destroy
+
+    validates :name, presence: true, length: { minimum: 3 }
+    validates :description, presence: true, length: { minimum: 3, maximum: 70 }
+    validates :services, presence: true, length: { minimum: 3 }
+    validates :img_url, presence: true
+    validates :price_nigth, presence: true
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :hotel
+  validates :title, presence: true, length: { minimum: 3 }
+
 end
