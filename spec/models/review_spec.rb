061fbc71acc149334875984d2b4e7f2d6dd2a070
Diego <diegodsha@hotmail.com>
2021-08-10 21:25:47 -0500

    Update review tests

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Review, type: :model do
   let(:name) { 'simple name' }
 
-  it { should belong_to(:hotel) } 
+  it { should belong_to(:user) } 
   it { should validate_presence_of(:title) }
 
 end
