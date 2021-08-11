d99a0b83bf21ee48fdb7bb4708a82492f675a21a
Diego <diegodsha@hotmail.com>
2021-08-11 18:31:49 -0500

    Fix cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://square-app.netlify.app/'
+    origins 'https://square-app.netlify.app'
 
     resource '*',
       headers: :any,
--- config/initializers/sessions_store.rb
+++ config/initializers/sessions_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-    Rails.application.config.session_store :cookie_store, key: '_square_app', domain: 'https://square-api.herokuapp.com'
+    Rails.application.config.session_store :cookie_store, key: '_square_app', domain: 'https://square-api-rr.herokuapp.com'
 else
     Rails.application.config.session_store :cookie_store, key: '_square_app'
 end
