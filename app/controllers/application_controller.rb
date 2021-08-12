fad87f9861c0954940fe2683583eed2bf3e01c8d
Diego <diegodsha@hotmail.com>
2021-08-12 14:35:37 -0500

    Enable cookies for production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,5 @@
 class ApplicationController < ActionController::API
-  # skip_before_action :verify_authenticity_token
-  # def current_user
-  #   User.find_by(id: session[:user_id])
-  # end
-
-  # def logged_in?
-  #   !current_user.nil?
-  # end
-  include ::ActionController::Cookies
+  include ActionController::Cookies
   include Rescue
   include SessionsHelper
-  # include CurrentUserConcern
 end
